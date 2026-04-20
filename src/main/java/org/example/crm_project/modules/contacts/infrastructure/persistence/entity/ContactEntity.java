0b75333141303eeede595e60fca41acddd93d479
baohc1705 <devblue404@gmail.com>
2026-04-20 11:25:43 +0700

    feat(contact): get all contact api

--- src/main/java/org/example/crm_project/modules/contacts/infrastructure/persistence/entity/ContactEntity.java
+++ src/main/java/org/example/crm_project/modules/contacts/infrastructure/persistence/entity/ContactEntity.java
@@ -1,4 +1,69 @@
 package org.example.crm_project.modules.contacts.infrastructure.persistence.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "contacts")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
 public class ContactEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "full_name")
+    private String fullName;
+
+    @Column(name = "position")
+    private String position;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "address")
+    private String address;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @Column(name = "dob")
+    private LocalDate dob;
+
+    @Column(name = "notes")
+    private String notes;
+
+    @Column(name = "is_primary")
+    private boolean isPrimary;
+
+    @Column(name = "created_by")
+    private Long createdBy;
+
+    @Column(name = "updated_by")
+    private Long updatedBy;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "is_active")
+    private boolean isActive = true;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private CustomerEntity customer;
 }
--- src/main/java/org/example/crm_project/modules/contacts/presentation/controller/ContactController.java
+++ src/main/java/org/example/crm_project/modules/contacts/presentation/controller/ContactController.java
@@ -1,4 +1,25 @@
 package org.example.crm_project.modules.contacts.presentation.controller;
 
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.example.crm_project.modules.contacts.application.dto.response.ContactResponse;
+import org.example.crm_project.modules.contacts.application.interfaces.ContactService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/contacts")
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class ContactController {
+    ContactService contactService;
+    @GetMapping
+    public ResponseEntity<List<ContactResponse>> findAll() {
+        return ResponseEntity.ok(contactService.findAll());
+    }
 }
