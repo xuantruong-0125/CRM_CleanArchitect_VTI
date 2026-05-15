ab6cf1f9b15711fc1dc7be995e291b043e2f4245
Xuan Truong <giamoc125@gmail.com>
2026-05-15 15:53:00 +0700

    delete temp module

--- src/main/java/org/example/crm_project/modules/leads/domain/exception/InvalidLeadException.java
+++
@@ -1,8 +0,0 @@
-package org.example.crm_project.modules.leads.domain.exception;
-
-public class InvalidLeadException extends RuntimeException {
-
-    public InvalidLeadException(String message) {
-        super(message);
-    }
-}
--- src/main/java/org/example/crm_project/modules/leads/domain/exception/LeadNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package org.example.crm_project.modules.leads.domain.exception;
-
-public class LeadNotFoundException extends RuntimeException {
-
-    public LeadNotFoundException(Long id) {
-        super("Lead not found with id: " + id);
-    }
-}
